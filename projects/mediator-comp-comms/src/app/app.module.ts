99a484eaede997a1d70a64931ba92295894cbb56
Santosh Marigowda <santosharakere@gmail.com>
2020-04-10 18:27:18 +0100

    added order and price-quoter components

--- projects/mediator-comp-comms/src/app/app.module.ts
+++ projects/mediator-comp-comms/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PriceQuoterComponent } from './price-quoter/price-quoter.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceQuoterComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/mediator-comp-comms/src/app/price-quoter/price-quoter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-quoter',
+  templateUrl: './price-quoter.component.html',
+  styleUrls: ['./price-quoter.component.scss']
+})
+export class PriceQuoterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

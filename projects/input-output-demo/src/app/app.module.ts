8a9caca08f408bc3261bf88530745b850c8e25eb
Santosh Marigowda <santosharakere@gmail.com>
2020-04-10 11:44:50 +0100

    Order Processor Component created

--- projects/input-output-demo/src/app/app.module.ts
+++ projects/input-output-demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderProcessorComponent } from './order-processor/order-processor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderProcessorComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/input-output-demo/src/app/order-processor/order-processor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-processor',
+  templateUrl: './order-processor.component.html',
+  styleUrls: ['./order-processor.component.scss']
+})
+export class OrderProcessorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

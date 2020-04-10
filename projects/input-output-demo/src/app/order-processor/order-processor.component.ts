6b4918aa07b3e6f4a29f84a002722a7cb2f62448
Santosh Marigowda <santosharakere@gmail.com>
2020-04-10 13:26:36 +0100

    using getter and setter  to  intercept the change when the  value is set and get by the UI

--- projects/input-output-demo/src/app/order-processor/order-processor.component.ts
+++ projects/input-output-demo/src/app/order-processor/order-processor.component.ts
@@ -7,8 +7,20 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class OrderProcessorComponent implements OnInit {
-  @Input() stockSymbol: string;
+  private _stockSymbol: string;
+  // @Input() stockSymbol: string;
   @Input() quantity: number;
+
+  @Input() set stockSymbol(value: string) {
+    this._stockSymbol = value;
+    if(!!value) {
+      this._stockSymbol = value;
+      console.log(`Buying ${this.quantity} shared of ${value}`);
+    }
+  }
+  get stockSymbol() {
+    return this._stockSymbol;
+  }
   constructor() {}
 
   ngOnInit(): void {}

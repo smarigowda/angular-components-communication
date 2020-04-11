0f053caefc0d461709e58dacc7b55546097e3c17
Santosh Marigowda <santosharakere@gmail.com>
2020-04-11 15:27:03 +0100

    using ngOnChanges in child component

--- projects/change-detection/src/app/child/child.component.ts
+++ projects/change-detection/src/app/child/child.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
   @Input() greeting: string;
   @Input() user: { name: string };
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: { [key: string]: SimpleChange }) {
+    console.log(JSON.stringify(changes, null, 2));
   }
-
 }

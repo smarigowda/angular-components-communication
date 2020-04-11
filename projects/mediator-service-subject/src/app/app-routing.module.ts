2508c6acaa77e1446d6d86b603a91ff19ae4baa2
Santosh Marigowda <santosharakere@gmail.com>
2020-04-11 09:52:52 +0100

    components created and routes configured.

--- projects/mediator-service-subject/src/app/app-routing.module.ts
+++ projects/mediator-service-subject/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EbayComponent } from './ebay/ebay.component';
+import { AmazonComponent } from './amazon/amazon.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EbayComponent },
+  { path: 'amazon', component: AmazonComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

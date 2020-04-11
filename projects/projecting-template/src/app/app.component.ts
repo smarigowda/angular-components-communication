c6c911aa23efa5451a97aeaf2b877b768f9b2db1
Santosh Marigowda <santosharakere@gmail.com>
2020-04-11 12:34:37 +0100

    projecting to named slots, multiple slots

--- projects/projecting-template/src/app/app.component.ts
+++ projects/projecting-template/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
-  title = 'projecting-template';
+  todaysDate = new Date().toLocaleDateString();
 }

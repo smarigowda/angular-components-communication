663fb5b56e329f88ecaf92b4adfef5f8442c53d0
Santosh Marigowda <santosharakere@gmail.com>
2020-04-11 06:54:55 +0100

    Search, eBay and Amazon components created

--- projects/mediator-service-subject/src/app/app.module.ts
+++ projects/mediator-service-subject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { EbayComponent } from './ebay/ebay.component';
+import { AmazonComponent } from './amazon/amazon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    EbayComponent,
+    AmazonComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/mediator-service-subject/src/app/ebay/ebay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ebay',
+  templateUrl: './ebay.component.html',
+  styleUrls: ['./ebay.component.scss']
+})
+export class EbayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

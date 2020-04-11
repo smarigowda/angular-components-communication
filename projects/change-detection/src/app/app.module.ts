1b496409ea494f8c62e603b1abcf9e49512f71ab
Santosh Marigowda <santosharakere@gmail.com>
2020-04-11 15:18:41 +0100

    Child component with 2 input bindings

--- projects/change-detection/src/app/app.module.ts
+++ projects/change-detection/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
